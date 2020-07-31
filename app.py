955d4b5efdf3051f28dd9a73f8af3297b88dccfd
Austin0000 <achanorsini@gmail.com>
2020-07-31 14:29:10 +0000

    deploy

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+   #         <input type="submit" value="Submit"/>
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
+from flask import render_template
+from flask import request
+from model import *
+from datetime import datetime
 
 # -- Initialization section --
 app = Flask(__name__)
@@ -13,4 +15,30 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html", time=datetime.now())
+
+@app.route('/results', methods= ['GET', 'POST'])
+def results():
+    meat = request.form["meat"]
+    meal = user_meat(meat)
+    return render_template("results.html", meal=meal, meat=meat, time=datetime.now())
+
+
+@app.route('/course', methods= ['GET', 'POST'])
+def course():
+    course = request.form["course"]
+    food = user_course(course)
+    return render_template("course.html", course=course, food=food,time=datetime.now())
+
+@app.route('/health', methods= ['GET', 'POST'])
+def health():
+    health = request.form["health"]
+    sweet = user_health(health)
+    return render_template("health.html", health=health, sweet=sweet,time=datetime.now())
+
+
+@app.route('/recipe', methods= ['GET', 'POST'])
+def recipe():
+    recipe = request.form["recipe"]
+    final = user_meat(recipe)
+    return render_template("recipe.html", recipe=recipe, final=final, time=datetime.now())
---
+++ model.py
@@ -0,0 +1,64 @@
+
+def user_course(course):
+    if course.lower() == "breakfast":
+        return "You chose breakfast."
+    elif course.lower() == "lunch":
+        return "You chose lunch."
+    elif course.lower() == "dinner":
+        return "You chose dinner."
+    else:
+        return "That wasn't an option..."
+        
+def user_meat(meat):
+    if meat:
+        return "Do you want breakfast, lunch or dinner?"
+    else:
+        return "you are not in the mood for meat."
+
+def user_recipe(recipe):
+    if recipe:
+        return "Do you want breakfast, lunch or dinner?"
+    else:
+        return "you are not in the mood for meat."
+
+def user_health(health):
+    if health.lower() == "savory":
+        return "Savory it is!"
+    elif health.lower() == "sweet":
+        return "Sweet it is!"
+    else:
+        return "That wasn't an option..."
+
+def user_recipe(recipe):
+    if user_meat == "yes":
+        if user_course == "breakfast":
+            if user_health == "savory":
+                return "https://www.foodnetwork.com/recipes/food-network-kitchen/breakfast-burritos-with-chorizo-3677254"
+            elif user_health == "sweet":
+                return "https://www.seriouseats.com/recipes/2017/12/bravetart-homemade-cinnamon-rolls-recipe.html"
+        elif user_course == "dinner":
+            if user_health == "savory":
+                return "https://www.foodandwine.com/recipes/bacon-and-kimchi-burgers"
+            elif user_health == "sweet":
+                return "https://ohsweetbasil.com/cookies-and-cream-red-velvet-molten-lava-cakes-recipe/"
+        elif user_course == "lunch":
+            if user_health == "savory":
+                return "https://natashaskitchen.com/fish-tacos-recipe/"
+            elif user_health == "sweet":
+                return "https://www.allrecipes.com/recipe/49943/grilled-peanut-butter-and-jelly-sandwich/"
+    elif user_meat == "no":
+        if user_course == "breakfast":
+            if user_health == "savory":
+                return "https://www.foodnetwork.com/recipes/food-network-kitchen/avocado-toasts-3364588"
+            elif user_health == "sweet":
+                return "https://cookieandkate.com/whole-wheat-banana-pancakes-recipe/"
+        elif user_course == "dinner":
+            if user_health == "savory":
+                return "https://www.feastingathome.com/orecchiette-pasta-with-broccoli-sauce/
+            elif user_health == "sweet":
+                return "https://minimalistbaker.com/banana-cream-pie-vegan-gf/"
+        elif user_course == "lunch":
+            if user_health == "savory":
+                return "https://www.acouplecooks.com/coleslaw-swiss-cheese-melt-sandwich/"
+            elif user_health == "sweet":
+                return "https://cookieandkate.com/vegan-mac-and-cheese-recipe/"
